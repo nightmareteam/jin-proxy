895185cf08f1511773d5c2da6b5a055fe495a332
Jin Cai <jinruncai@gmail.com>
2020-04-11 13:44:20 -0700

    Add redirect request utility

---
+++ server/utils/redirectRequest.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+const redirectRequest = ({
+    originalURL,
+    headers,
+    method,
+    params,
+    data,
+}, domain) => (
+    axios({
+        url: `${domain}${originalUrl}`,
+        params,
+        method,
+        data,
+        headers: {
+            ...headers,
+            'Cache-Control': 'no-cache'
+        }
+    })
+);
+
+module.exports = redirectRequest;
