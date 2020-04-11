635762c3399b210a089720bdc3391263622a1827
Jin Cai <jinruncai@gmail.com>
2020-04-11 14:09:44 -0700

    Add route to redirect requests to services

--- server/server.js
+++ server/server.js
@@ -1,4 +1,19 @@
 const express = require('express');
+const DOMAINS = require('./constants/DOMAINS');
+const redirectRequest = require('./utils/redirectRequest')
+
 const app = express();
+
+app.get('/api/:service', (req, res) => {
+    const { service } = req.params;
+    const { [service]: domain } = DOMAINS;
+    redirectRequest(req, domain)
+        .then(({ data }) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.status(500).send(err);
+        });
+});
 
 module.exports = app;
--- server/utils/redirectRequest.js
+++ server/utils/redirectRequest.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const redirectRequest = ({
-    originalURL,
+    originalUrl,
     headers,
     method,
     params,
