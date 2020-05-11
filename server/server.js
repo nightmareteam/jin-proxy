5247352f210ad7e9460ce7226a9d11cafa4c48f1
Jin Cai <jinruncai@gmail.com>
2020-05-11 12:25:45 -0700

    Add static bundle

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
+const path = require('path');
+const axios = require('axios');
 const express = require('express');
 const DOMAINS = require('./constants/DOMAINS');
 const redirectRequest = require('./utils/redirectRequest')
 
 const app = express();
 
-app.get('/api/:service/*', (req, res) => {
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
+
+app.get('/:service/*', (req, res) => {
     const { service } = req.params;
     const { [service]: domain } = DOMAINS;
     redirectRequest(req, domain)
@@ -12,6 +16,7 @@ app.get('/api/:service/*', (req, res) => {
             res.send(data);
         })
         .catch((err) => {
+            console.log(err);
             res.status(500).send(err);
         });
 });
